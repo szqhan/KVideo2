3656cc886aba22ccdcce778fa099967fc9acf624
szqhan <59555166+szqhan@users.noreply.github.com>
2026-02-27 16:42:31 +0800

    Downgrade Android Gradle plugin version
    Updated Android Gradle plugin version from 8.2.0 to 8.1.0.

--- android-tv/build.gradle.kts
+++ android-tv/build.gradle.kts
@@ -1,4 +1,10 @@
 plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.android.application") version "8.1.0" apply false
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
